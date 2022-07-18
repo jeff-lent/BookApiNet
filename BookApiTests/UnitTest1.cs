d01f4fe3d6af856982309408288b556c1171cea5
jeff-lent <jefflent01@gmail.com>
2022-07-18 00:27:38 -0400

    Initial commit

---
+++ BookApiTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace BookApiTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+}
